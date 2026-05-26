05657096945676773db9919c76ea76d2dc1dec65
Mitchellmwf <mitchellmwf@gmail.com>
2026-05-26 17:04:52 -0400

    Fill out subjects controller

--- Assignment1/Controllers/SubjectController.cs
+++ Assignment1/Controllers/SubjectController.cs
@@ -1,21 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Assignment1.Data;
+using CourseBudd.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CourseBudd.Controllers
 {
     public class SubjectController : Controller
     {
+        //shared db conn
+        private readonly ApplicationDbContext _context;
+
+        //Constructor w/dp dependency 
+        public SubjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            var subjects = new List<CourseBudd.Models.Subject>();
-            return View(subjects);
+            var subject = _context.Subject.ToList();
+            return View(subject);
         }
         public IActionResult Create()
         {
             return View();
         }
-        public IActionResult Edit()
+
+        [HttpPost]
+        public IActionResult Create([Bind("SubjectId,Name,Description")] CourseBudd.Models.Subject subject)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View(subject);
+            }
+            // Create new subject & save to database
+            _context.Subject.Add(subject);
+            _context.SaveChanges();
+
+            // Redirect to Index to see update list of categories
+            return RedirectToAction(nameof(Index));
         }
+        public IActionResult Edit(int id)
+        {
+            // find subject to edit
+            var subject = _context.Subject.Find(id);
+
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind("SubjectId, Name, Description")] Subject subject)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(subject);
+            }
+            // Update subject in database
+            _context.Subject.Update(subject);
+            _context.SaveChanges();
+            // Redirect to Index to see update list of subjects
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int id)
+        {
+            // find subject to delete
+            var subject = _context.Subject.Find(id);
+
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            _context.Subject.Remove(subject);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
